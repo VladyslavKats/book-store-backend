73734d27b283ec4cfd10ba0ab3f8b42fcbd32015
VladyslavKats <kac9661@gmail.com>
2022-11-17 22:55:48 +0200

    add context

---
+++ BookStore/BookStore.DAL/EF/BookStoreContext.cs
@@ -0,0 +1,51 @@
+﻿using BookStore.DAL.Configurations;
+using BookStore.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.DAL.EF
+{
+    public class BookStoreContext : DbContext
+    {
+        public BookStoreContext(DbContextOptions<BookStoreContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<BookEntity> Books { get; set; }
+
+        public DbSet<AuthorEntity> Authors { get; set; }
+
+        public DbSet<GenreEntity> Genres { get; set; }
+
+        public DbSet<LanguageEntity> Languages { get; set; }
+
+        public DbSet<CommentEntity> Comments { get; set; }
+
+        public DbSet<OrderEntity> Orders { get; set; }
+
+        public DbSet<OrderDetailEntity> OrderDetails { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(BookConfiguration).Assembly);
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            SetCreatedAtField<BookEntity>();
+            SetCreatedAtField<CommentEntity>();
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+        private void SetCreatedAtField<TEntity>() where TEntity : EntityWithCreatedAtField
+        {
+            var entities = ChangeTracker
+                .Entries<TEntity>()
+                .Where(e => e.State == EntityState.Added);
+            foreach (var entity in entities)
+            {
+                entity.Entity.CreatedAt = DateTime.UtcNow;
+            }
+        }
+    }
+}
